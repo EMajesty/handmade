eb366e6545259916fbbdffd8565ee5b5faa8c971
EMajesty <lassi.laitinen@protonmail.com>
2025-04-07 20:51:10 +0300

    hell world

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 # Compiler and flags
 CXX = g++
-CXXFLAGS = -Wall -Wextra -std=c++17 -Iinclude -g
+CXXFLAGS = -Wall -Wextra -std=c++17 -Iinclude -g -lSDL2
 
 # Project directories
 SRC_DIR = src
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
+#include <SDL2/SDL.h>
 
 int main(int argc, char **argv) {
     std::cout << "Hell world" << std::endl;
-    return 0;
+    
+    SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_INFORMATION, "Hell World", "Hell World", 0);
+    return(0);
 }
